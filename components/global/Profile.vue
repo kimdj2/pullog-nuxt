86f25a04744d7345d01893d3acf8bd7ab394e5b0
kim dongjin <1222kdj@gmail.com>
2020-12-12 17:02:54 +0900

    modify profile

--- components/global/Profile.vue
+++ components/global/Profile.vue
@@ -1,13 +1,13 @@
 <template>
   <v-card>
     <v-card-text>
-      <v-row align="center">
-        <v-col cols="12">
+      <v-row align="center" justify="center" align-content="center">
+        <v-col cols="12" class="text-center" align="center">
           <v-avatar size="65">
             <v-img :src="require('@/assets/Kim_Dongjin.png')"></v-img>
           </v-avatar>
         </v-col>
-        <v-col cols="12" class="text-right">
+        <v-col cols="12" class="text-center" align="center">
           <v-icon>mdi-email</v-icon>
           1222kdj@gmail.com
         </v-col>
