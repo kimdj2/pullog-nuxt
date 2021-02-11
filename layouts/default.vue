c2b854936a080de3cc425a05c5a106e548428c77
kim dongjin <1222kdj@gmail.com>
2021-02-11 14:50:16 +0900

    tui editor contents font size

--- layouts/default.vue
+++ layouts/default.vue
@@ -104,7 +104,7 @@ export default {
   font-family: 'M Plus 1p', 'Open Sans', 'Helvetica Neue', 'Helvetica', 'Arial',
     '나눔바른고딕', 'Nanum Barun Gothic', '맑은고딕', 'Malgun Gothic',
     sans-serif !important;
-  font-size: 14px;
+  font-size: 14px !important;
   pre {
     background-color: #f8f8f8;
   }
